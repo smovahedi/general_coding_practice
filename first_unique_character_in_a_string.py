2a4c8f8990aafc64701dc780aaf1e0c863ba3ed9
smovahedi <movahedisara@yahoo.com>
2020-11-27 09:06:06 +0000

    Update first_unique_character_in_a_string.py
    returning index

--- first_unique_character_in_a_string.py
+++ first_unique_character_in_a_string.py
@@ -9,3 +9,16 @@ for ch in mystring:
     if repdic[ch.lower()]==1: 
         print(ch) 
         exit()
+
+# class Solution:
+#     def firstUniqChar(self, s: str) -> int:
+#         repdic = {}
+#         for ch in s: 
+#             try:
+#                 repdic[ch.lower()]+=1
+#             except:
+#                 repdic[ch.lower()]=1
+#         for chi in range(len(s)): 
+#             if repdic[s[chi].lower()]==1: 
+#                 return(chi)      
+#         return(-1)
